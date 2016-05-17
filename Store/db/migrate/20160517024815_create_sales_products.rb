c721517825609913f90ce38678a0701849381363
abelorihuelamendoza <abelorihuelamendoza@hotmail.com>
2016-05-17 12:10:40 -0500

    Update migration order elememnts

--- Store/db/migrate/20160517024815_create_sales_products.rb
+++ Store/db/migrate/20160517024815_create_sales_products.rb
@@ -2,7 +2,7 @@ class CreateSalesProducts < ActiveRecord::Migration
   def change
     create_table :products_sales, id: false do |t|
       t.belongs_to :product, index: true
-      t.belongs_to :sale, index: true
+      t.belongs_to :sale, index: tru
     end
   end
 end
